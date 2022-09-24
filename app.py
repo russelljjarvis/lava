3b8e2527bae5ecec3fc7db30a87a90687872cf77
Russell Jarvis <russelljarvis@protonmail.com>
2022-09-24 17:49:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,50 @@ import streamlit as st
 
 st.title("Build works?")
 
+from lava.proc.learning_rules.stdp_learning_rule import STDPLoihi
+
+
+import numpy as np
+
+# Set this tag to "fixed_pt" or "floating_pt" to choose the corresponding models.
+SELECT_TAG = "floating_pt"
+
+# LIF parameters
+if SELECT_TAG == "fixed_pt":
+    du = 4095
+    dv = 4095
+elif SELECT_TAG == "floating_pt":
+    du = 1
+    dv = 1
+vth = 240
+
+# Number of neurons per layer
+num_neurons = 1
+shape_lif = (num_neurons, )
+shape_conn = (num_neurons, num_neurons)
+
+# Connection parameters
+
+# SpikePattern -> LIF connection weight
+wgt_inp = np.eye(num_neurons) * 250
+
+# LIF -> LIF connection initial weight (learning-enabled)
+wgt_plast_conn = np.full(shape_conn, 50)
+    
+# Number of simulation time steps
+num_steps = 200
+time = list(range(1, num_steps + 1))
+
+# Spike times
+spike_prob = 0.03
+
+# Create spike rasters
+np.random.seed(123)
+spike_raster_pre = np.zeros((num_neurons, num_steps))
+np.place(spike_raster_pre, np.random.rand(num_neurons, num_steps) < spike_prob, 1)
+
+spike_raster_post = np.zeros((num_neurons, num_steps))
+np.place(spike_raster_post, np.random.rand(num_neurons, num_steps) < spike_prob, 1)
+
+
 
