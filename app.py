000ee3c8ba5694c79b9bfb11ab9276b52bedc73b
Russell Jarvis <russelljarvis@protonmail.com>
2022-09-24 18:02:34 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import lava
 import streamlit as st
 
-st.title("Build works?")
+st.title("Build apparently works!")
 
 from lava.proc.learning_rules.stdp_learning_rule import STDPLoihi
 
 
 import numpy as np
+from lava.proc.lif.process import LIF
+from lava.proc.io.source import RingBuffer
+from lava.proc.dense.process import LearningDense as Dense
+
 
 # Set this tag to "fixed_pt" or "floating_pt" to choose the corresponding models.
 SELECT_TAG = "floating_pt"
@@ -48,5 +52,63 @@ np.place(spike_raster_pre, np.random.rand(num_neurons, num_steps) < spike_prob, 
 spike_raster_post = np.zeros((num_neurons, num_steps))
 np.place(spike_raster_post, np.random.rand(num_neurons, num_steps) < spike_prob, 1)
 
+stdp = STDPLoihi(learning_rate=1,
+                 A_plus=-1,
+                 A_minus=1,
+                 tau_plus=10,
+                 tau_minus=10,
+                 t_epoch=2)
 
+
+
+# Create input devices
+pattern_pre = RingBuffer(data=spike_raster_pre.astype(int))
+pattern_post = RingBuffer(data=spike_raster_post.astype(int))
+
+# Create input connectivity
+conn_inp_pre = Dense(weights=wgt_inp)
+conn_inp_post = Dense(weights=wgt_inp)
+
+# Create pre-synaptic neurons
+lif_pre = LIF(u=0,
+              v=0,
+              du=du,
+              dv=du,
+              bias_mant=0,
+              bias_exp=0,
+              vth=vth,
+              shape=shape_lif,
+              name='lif_pre')
+
+# Create plastic connection
+plast_conn = Dense(weights=wgt_plast_conn,
+                   learning_rule=stdp,
+                   name='plastic_dense')
+
+# Create post-synaptic neuron
+lif_post = LIF(u=0,
+               v=0,
+               du=du,
+               dv=du,
+               bias_mant=0,
+               bias_exp=0,
+               vth=vth,
+               shape=shape_lif,
+               name='lif_post')
+
+# Connect network
+pattern_pre.s_out.connect(conn_inp_pre.s_in)
+conn_inp_pre.a_out.connect(lif_pre.a_in)
+
+pattern_post.s_out.connect(conn_inp_post.s_in)
+conn_inp_post.a_out.connect(lif_post.a_in)
+
+lif_pre.s_out.connect(plast_conn.s_in)
+plast_conn.a_out.connect(lif_post.a_in)
+
+# Connect back-propagating actionpotential (BAP)
+lif_post.s_out.connect(plast_conn.s_in_bap)
+
+
+st.title("Build apparently works?")
 
